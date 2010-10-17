26b3270061d6007ed8e2facd465cfe39bc3f8ffe
abhirama <none@none>
2010-10-17 14:00:56 +0530

    Directory traverser takes a white list of file extensions

--- src/com/abhyrama/smushit/FileTraverser.java
+++ src/com/abhyrama/smushit/FileTraverser.java
@@ -8,15 +8,30 @@
 package com.abhyrama.smushit;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class FileTraverser {
   protected final String rootDirectory;
+  protected final Set<String> validFileExtensions;
+
+  protected final boolean isFileExtensionListSupplied;
+
   protected List<String> files = new LinkedList<String>();
 
-  public FileTraverser(String rootDirectory) {
+  public FileTraverser(String rootDirectory, Set<String> validFileExtension) {
     this.rootDirectory = rootDirectory;
+    //if this is empty we assume all files are valid
+    this.validFileExtensions = validFileExtension;
+
+    if (this.validFileExtensions != null && this.validFileExtensions.size() > 0) {
+      this.isFileExtensionListSupplied = true;
+    } else {
+      this.isFileExtensionListSupplied = false;
+    }
   }
 
   public List<String> getFiles() {
@@ -30,13 +45,32 @@ public class FileTraverser {
       if (file.isDirectory()) {
         this.getFilesHelper(file);
       } else {
-        this.files.add(file.getAbsolutePath());
+        if (this.isAcceptedFile(file)) {
+          this.files.add(file.getAbsolutePath());
+        }
       }
     }
   }
 
+  protected boolean isAcceptedFile(File file) {
+    if (this.isFileExtensionListSupplied) {
+      return this.validFileExtensions.contains(this.getExtension(file));
+    }
+
+    //if the extension while list is not defined it means all files have to be listed
+    return true;
+  }
+
+  protected String getExtension(File file) {
+    String filename = file.toString();
+    String ext = filename.substring(filename.lastIndexOf(".") + 1, filename.length());
+    return ext;
+  }
+
   public static void main(String[] args) {
-    FileTraverser fileTraverser = new FileTraverser("D:\\projects\\personal\\smushit");
+    Set<String> validFiles = new HashSet<String>();
+    validFiles.add("java");
+    FileTraverser fileTraverser = new FileTraverser("D:\\projects\\personal\\smushit", validFiles);
     System.out.println(fileTraverser.getFiles());
   }
 }
