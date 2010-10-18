435372d50498bf9f96be677797aca83f183b0de2
abhirama <none@none>
2010-10-18 15:53:25 +0530

    Using a FileFilter to filter files in a directory

--- src/com/abhyrama/smushit/FileTraverser.java
+++ src/com/abhyrama/smushit/FileTraverser.java
@@ -8,6 +8,7 @@
 package com.abhyrama.smushit;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,25 +16,13 @@ import java.util.Set;
 
 public class FileTraverser {
   protected final String rootDirectory;
-  //this has to be in lower case
-  protected final Set<String> validFileExtensions;
-
-  protected final boolean isFileExtensionListSupplied;
-  protected final int maximumFileSize;
+  protected final FileFilter fileFilter;
 
   protected List<String> files = new LinkedList<String>();
 
-  public FileTraverser(String rootDirectory, Set<String> validFileExtension, int maximumFileSize) {
+  public FileTraverser(String rootDirectory, FileFilter fileFilter) {
     this.rootDirectory = rootDirectory;
-    //if this is empty we assume all files are valid
-    this.validFileExtensions = validFileExtension;
-    this.maximumFileSize = maximumFileSize;
-
-    if (this.validFileExtensions != null && this.validFileExtensions.size() > 0) {
-      this.isFileExtensionListSupplied = true;
-    } else {
-      this.isFileExtensionListSupplied = false;
-    }
+    this.fileFilter = fileFilter;
   }
 
   public List<String> getFiles() {
@@ -42,40 +31,27 @@ public class FileTraverser {
   }
 
   protected void getFilesHelper(File directory) {
-    File[] files = directory.listFiles();
+    File[] files;
+    if (this.fileFilter != null) {
+      files = directory.listFiles(this.fileFilter);
+    } else {
+      files = directory.listFiles();
+    }
+
     for (File file : files) {
       if (file.isDirectory()) {
         this.getFilesHelper(file);
       } else {
-        if (this.isAcceptedFile(file)) {
-          this.files.add(file.getAbsolutePath());
-        }
+        this.files.add(file.getAbsolutePath());
       }
     }
-  }
-
-  protected boolean isAcceptedFile(File file) {
-    return this.isAcceptedFileExtension(file) && this.isWithinFileSize(file);
-  }
-
-  protected boolean isAcceptedFileExtension(File file) {
-    if (this.isFileExtensionListSupplied) {
-      return this.validFileExtensions.contains(Utils.getExtension(file.toString()));
-    }
-
-    //if the extension while list is not defined it means all files have to be listed
-    return true;
-  }
-
-  protected boolean isWithinFileSize(File file) {
-    return file.length() <= this.maximumFileSize;
   }
 
   public static void main(String[] args) {
     Set<String> validFiles = new HashSet<String>();
     validFiles.add("jpg");
     validFiles.add("jpeg");
-    FileTraverser fileTraverser = new FileTraverser("D:\\projects\\burrp\\local\\mobile-api-branch\\web\\images\\nye\\download", validFiles, 1000000);
+    FileTraverser fileTraverser = new FileTraverser("D:\\projects\\burrp\\local\\mobile-api-branch\\web\\images\\nye\\download", new MyFileFilter(validFiles, SmushIt.MAX_FILE_SIZE));
     System.out.println(fileTraverser.getFiles());
   }
 }
---
+++ src/com/abhyrama/smushit/MyFileFilter.java
@@ -0,0 +1,46 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: abhirama
+ * Date: Oct 18, 2010
+ * Time: 3:23:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+package com.abhyrama.smushit;
+
+import java.io.File;
+import java.util.Set;
+
+public class MyFileFilter implements java.io.FileFilter {
+  protected final Set<String> acceptedFileExtensions;
+  protected final int maximumFileSize;
+  protected final boolean isFileExtensionListSupplied;
+
+  public MyFileFilter(Set<String> acceptedFileExtensions, int maximumFileSize) {
+    this.acceptedFileExtensions = acceptedFileExtensions;
+    this.maximumFileSize = maximumFileSize;
+
+    if (this.acceptedFileExtensions != null && this.acceptedFileExtensions.size() > 0) {
+      this.isFileExtensionListSupplied = true;
+    } else {
+      this.isFileExtensionListSupplied = false;
+    }
+  }
+
+  public boolean accept(File file) {
+    return this.isAcceptedFileExtension(file) && this.isWithinFileSize(file);
+  }
+
+  protected boolean isAcceptedFileExtension(File file) {
+    if (this.isFileExtensionListSupplied) {
+      return this.acceptedFileExtensions.contains(Utils.getExtension(file.toString()));
+    }
+
+    //if the extension while list is not defined it means all files have to be listed
+    return true;
+  }
+
+  protected boolean isWithinFileSize(File file) {
+    System.out.println(file.length());
+    return file.length() <= this.maximumFileSize;
+  }
+}
